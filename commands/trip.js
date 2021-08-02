8f20affbc4dc65ee1b12abe81507d6f085e9f59a
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-08-01 20:07:32 -0700

    Add trip command embed

--- commands/trip.js
+++ commands/trip.js
@@ -8,6 +8,7 @@ module.exports = {
         const mongoose = require('mongoose');
         const Trip = require('../config/trip-schema');
 
+
         if (args[0] === 'add') {
 
             new Trip ({
@@ -17,30 +18,19 @@ module.exports = {
                 message.channel.send('New trip: ' + newTrip);
             });
 
-
-
-/*
-            message.channel.send('<:dlineleft:777449590718070795><:dlinecenter:777449590974185474><:dlineright:777449591179182110> <:dscord:777453385443573771> ***Server updates — December 18, 2020*** <:dscord:777453385443573771> <:dlineleft:777449590718070795><:dlinecenter:777449590974185474><:dlineright:777449591179182110>');
-            const dec182020ClubsEmbed = new Discord.MessageEmbed()
-                .setColor('#ff3300')
-                .setTitle('NEW: Clubs channel')
-                .setDescription('It\'s a small list at the moment, but we now have a place for d.games-supported clubs! Check em out and sign up :)')
-                .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/house-with-garden_1f3e1.png');
-            const dec182020InterestsEmbed = new Discord.MessageEmbed()
-                .setColor('#ff3300')
-                .setTitle('NEW: Interests channel')
-                .setDescription('Into something but don\'t have time to commit to a club, or want a quicker way of meeting people who are into the same thing? Check out #interests!')
-                .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/259/busts-in-silhouette_1f465.png');
-            message.channel.send(dec182020ClubsEmbed)
-                .then(message.channel.send(dec182020InterestsEmbed));
-*/
-
-
-
-
+        } else if (!args[0]) {
+            const tripCommandEmbed = new Discord.MessageEmbed()
+                .setColor('#ff0cff')
+                .setTitle('Costco trip command')
+                .setDescription('Used to create new trips and roles we can track/mention')
+                .addField('add', 'r.trip add <name of trip> <date>', false)
+                .addField('remove', 'r.trip remove <name of trip>', false)
+                .addField('reschedule', 'r.trip rs <name of trip> <new date>', false);
+            message.channel.send(tripCommandEmbed);
 
         } else if (args[0] !== 'add') {
             message.channel.send('The command does not yet support trip modification/deletion!');
         }
+
     }
 };
