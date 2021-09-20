7f76af0878cc87239af13941ecd43e35f6aa4150
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-20 14:09:48 -0700

    Listen for basic interactions

--- index.js
+++ index.js
@@ -53,6 +53,21 @@ client.on('messageCreate', async message => {
     console.log(error);
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    } else if (commandName === 'server') {
+        await interaction.reply('Server info.');
+    } else if (commandName === 'user') {
+        await interaction.reply('User info.');
+    }
+});
+
+
 // ****************************** REACTION LISTENERS ******************************
 
 client.on('messageReactionAdd', async (reaction, user) => {
