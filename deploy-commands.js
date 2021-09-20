c1cefe1aac705c95286886368e805d8b3b65b048
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-20 15:56:33 -0700

    Delete obsolete commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,12 +16,14 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
+        console.log('Started refreshing application (/) commands.');
+
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
             { body: commands },
         );
 
-        console.log('Successfully registered application commands.');
+        console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
     }
