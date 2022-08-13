46a8ea4064e0345f9fecd1706c84bf6dca39b114
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2022-08-13 15:26:23 -0700

    Add events into one collection, sort by `guildId`

--- config/event-schema.js
+++ config/event-schema.js
@@ -1,15 +1,14 @@
-const eventCommand = require('../commands/event');
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
+    guildId: String,
     eventName: String,
     eventDate: String,
     eventEmoji: String,
     eventMessageId: String,
     eventRoleId: String
-}, { collection: eventCommand.guildId });
+});
 
 const Event = mongoose.model('event', eventSchema);
 
